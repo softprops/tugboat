31e28586e99c06d56f5705fd5610b60d27e8bb22
softprops <d.tangren@gmail.com>
2014-07-03 08:13:57 -0400

    use class name instead of extra field

--- src/main/scala/Port.scala
+++ src/main/scala/Port.scala
@@ -2,15 +2,14 @@ package tugboat
 
 sealed trait Port {
   def value: Int
-  def kind: String
+  def kind: String = getClass.getSimpleName.toLowerCase
   def spec = s"$value/$kind"
 }
 
 object Port {
   private[this] val Spec = """(\d+)/(.+)""".r
-  abstract class Kind(val kind: String) extends Port
-  case class Tcp(value: Int) extends Kind("tcp")
-  case class Udp(value: Int) extends Kind("udp")
+  case class Tcp(value: Int) extends Port
+  case class Udp(value: Int) extends Port
   def unapply(spec: String): Option[Port] = spec match {
     case Spec(value, "tcp") =>
       Some(Tcp(value.toInt))
